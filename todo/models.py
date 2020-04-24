56dfb3474e78ad9ab4903af21614478810791f2d
gal <tanjhu@163.com>
2020-04-24 20:36:45 +0800

    auto_now_add和auto_now-時間記錄

--- todo/models.py
+++ todo/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class TodoItem(models.Model):
     content =models.TextField()
+    create_time=models.DateTimeField(auto_now_add=True)
+    last_edit=models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.content
 
