94d4f56bf6fe46b0e4ada68933181baa5d8c3c91
gal <tanjhu@163.com>
2020-04-25 11:19:18 +0800

    修改models增加todo的類型

--- todo/models.py
+++ todo/models.py
@@ -1,11 +1,46 @@
 from django.db import models
+from django.utils import timezone
 
 class TodoItem(models.Model):
-    content =models.TextField()
+    Important = 0
+    MVP_Project = 1
+    Compound_Effect = 2
+    General = 3
+    Sub_General = 4
+    Emergence_Today =  5
+    Emergence_Week = 6
+    Emergece_Month = 7
+    Aim_Today =  8
+    Aim_Week= 9
+    Aim_Month = 10
+    THE_ONE_THING  = 11
+    TECH =  12
+    Remind =  13
+    Priorities = (
+                (Important,'重要事項(優先處理'),
+                (MVP_Project,'手頭最重要的項目(當紅炸子雞項目)'),
+                (Compound_Effect,'每天都堅持要做的事情(風雨無阻'),
+                (General,'一般'),
+                (Sub_General,'準一般'),
+                (Emergence_Today,'今日緊急項目'),
+                (Emergence_Week,'本周緊急項目'),
+                (Emergece_Month,'本月緊急'),
+                (Aim_Today,'今日目標'),
+                (Aim_Week,'本周目標'),
+                (Aim_Month,'本月目標'),
+                (THE_ONE_THING,'唯一重要的事情'),
+                (TECH,'技術盞'),
+                (Remind,'提醒'),)
+    content =models.TextField(default='今天目標是:')
+    period = models.DateField(default=timezone.now)
+    rating = models.IntegerField(
+            choices=Priorities,
+            default=Important)
+    runtime = models.PositiveIntegerField(default=1)
     create_time=models.DateTimeField(auto_now_add=True)
     last_edit=models.DateTimeField(auto_now=True)
     def __str__(self):
-        return self.content
+        return f'{self.content} {(self.period)}'
 
 
 class BinItem(models.Model):
